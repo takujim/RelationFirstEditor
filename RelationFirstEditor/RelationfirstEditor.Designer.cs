32457a5b4e41fb31498547161f7db555046c261e
宮本卓治 <takaki2222@gmail.com>
2017-08-01 13:58:45 +0900

    ver0.1

---
+++ RelationFirstEditor/RelationfirstEditor.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace RelationFirstEditor
+{
+    partial class RelationfirstEditor
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RelationfirstEditor));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lstAllFile = new System.Windows.Forms.ListBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.htmlView = new System.Windows.Forms.WebBrowser();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(61)))), ((int)(((byte)(63)))));
+            this.splitContainer1.Panel1.Controls.Add(this.btnEdit);
+            this.splitContainer1.Panel1.Controls.Add(this.lstAllFile);
+            this.splitContainer1.Panel1.Controls.Add(this.btnUpdate);
+            this.splitContainer1.Panel1.Controls.Add(this.btnOpen);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.htmlView);
+            this.splitContainer1.Size = new System.Drawing.Size(1028, 619);
+            this.splitContainer1.SplitterDistance = 240;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.White;
+            this.btnEdit.BackgroundImage = global::RelationFirstEditor.Properties.Resources.edit;
+            this.btnEdit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnEdit.Location = new System.Drawing.Point(123, 9);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(36, 36);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // lstAllFile
+            // 
+            this.lstAllFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstAllFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(61)))), ((int)(((byte)(63)))));
+            this.lstAllFile.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstAllFile.ForeColor = System.Drawing.Color.White;
+            this.lstAllFile.FormattingEnabled = true;
+            this.lstAllFile.HorizontalScrollbar = true;
+            this.lstAllFile.ItemHeight = 12;
+            this.lstAllFile.Location = new System.Drawing.Point(3, 60);
+            this.lstAllFile.Name = "lstAllFile";
+            this.lstAllFile.Size = new System.Drawing.Size(234, 552);
+            this.lstAllFile.TabIndex = 3;
+            this.lstAllFile.SelectedIndexChanged += new System.EventHandler(this.lstAllFile_SelectedIndexChanged);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.White;
+            this.btnUpdate.BackgroundImage = global::RelationFirstEditor.Properties.Resources.update;
+            this.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnUpdate.Location = new System.Drawing.Point(45, 9);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(36, 36);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.White;
+            this.btnOpen.BackgroundImage = global::RelationFirstEditor.Properties.Resources.open;
+            this.btnOpen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnOpen.Location = new System.Drawing.Point(3, 9);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(36, 36);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 6;
+            // 
+            // htmlView
+            // 
+            this.htmlView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.htmlView.Location = new System.Drawing.Point(0, 0);
+            this.htmlView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.htmlView.Name = "htmlView";
+            this.htmlView.Size = new System.Drawing.Size(784, 619);
+            this.htmlView.TabIndex = 0;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1028, 619);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RelationFirstEditor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.WebBrowser htmlView;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox lstAllFile;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RelationFirstEditor/RelationfirstEditor.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelationFirstEditor
+{
+    public partial class RelationfirstEditor : Form
+    {
+        public RelationfirstEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.SelectedPath = folderBrowserDialog1.SelectedPath;
+                Properties.Settings.Default.Save();
+
+                updateList();
+            }
+        }
+
+        private void updateList()
+        {
+            if (folderBrowserDialog1.SelectedPath != string.Empty)
+            {
+                lstAllFile.Items.Clear();
+                DirSearch(folderBrowserDialog1.SelectedPath, 0);
+            }
+        }
+
+        private void DirSearch(string sDir, int count)
+        {
+            count++;
+
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    //foreach (string f in Directory.GetFiles(d, txtFile.Text))
+                    //{
+                    //    lstFilesFound.Items.Add(f);
+                    //}
+                    if (count == 1)
+                    {
+                        lstAllFile.Items.Add("----------------");
+                    }
+                    lstAllFile.Items.Add(d.Replace(folderBrowserDialog1.SelectedPath, ""));
+                    DirSearch(d, count);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                MessageBox.Show(excpt.ToString());
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (isFolder())
+            {
+                System.Diagnostics.Process.Start(getSelectFolderPath());
+            }
+        }
+
+        private string getSelectFolderPath()
+        {
+            if (isFolder())
+            {
+                return folderBrowserDialog1.SelectedPath + lstAllFile.SelectedItem.ToString();
+            }
+
+            MessageBox.Show("Folder not found");
+            return "";
+        }
+
+        private bool isFolder()
+        {
+            return lstAllFile.SelectedIndex > -1 && lstAllFile.SelectedItem.ToString() != "----------------";
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            updateList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = Properties.Settings.Default.SelectedPath;
+            updateList();
+            htmlView.DocumentText = "Hello!!";
+        }
+
+        private void lstAllFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (isFolder())
+            {
+                var files = Directory.GetFiles(getSelectFolderPath(), "*.html");
+                if (files.Count() > 0)
+                {
+                    htmlView.Url = new Uri(files[0]);
+                }
+                else
+                {
+                    htmlView.DocumentText = "HTML file not found";
+                    //MessageBox.Show("HTML file not found");
+                }
+            }
+        }
+    }
+}
